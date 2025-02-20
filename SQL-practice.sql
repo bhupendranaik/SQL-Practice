8c1b27d35829925b7235ae2f2a36f9de87d35058
bhupendranaik <bhupendra.naik265@gmail.com>
2025-02-20 23:43:52 +0530

    Updated SQL-practice 3

--- SQL-practice.sql
+++ SQL-practice.sql
@@ -352,3 +352,129 @@ SELECT
 FROM (SELECT admission_date, COUNT(*) AS visits_n
       FROM admissions
       GROUP BY admission_date);
+      
+-- Q.43 Show all of the patients grouped into weight groups.
+-- Show the total amount of patients in each weight group. Order the list by the weight group decending.
+-- For example, if they weight 100 to 109 they are placed in the 100 weight group, 110-119 = 110 weight group, etc.
+-- Soln
+SELECT 
+    COUNT(*) AS amt_of_patients,
+    FLOOR(weight / 10) * 10 AS weight_group
+FROM patients
+GROUP BY weight_group
+ORDER BY weight_group DESC;
+
+-- Q.44 Show patient_id, weight, height, isObese from the patients table. Display isObese as a boolean 0 or 1.
+--      Obese is defined as weight(kg)/(height(m)2) >= 30. weight is in units kg. height is in units cm.
+-- Soln
+select patient_id, weight, height,
+(case 
+    when (weight/(power(height/100.0,2)))>=30 then 1
+    else 0
+ end) as isobese from patients;
+ 
+-- Q.45 Show patient_id, first_name, last_name, and attending doctor's specialty.
+--      Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
+--      Check patients, admissions, and doctors tables for required information.
+-- Soln
+SELECT p.patient_id, p.first_name, p.last_name, d.specialty
+FROM patients p
+JOIN admissions a ON p.patient_id = a.patient_id
+JOIN doctors d ON a.attending_doctor_id = d.doctor_id
+WHERE a.diagnosis = 'Epilepsy' AND d.first_name = 'Lisa';
+
+-- Q.46 All patients who have gone through admissions, can see their medical documents on our site. 
+--      Those patients are given a temporary password after their first admission. Show the patient_id and temp_password.
+--      The password must be the following, in order:
+--      1. patient_id
+--      2. the numerical length of patient's last_name
+--      3. year of patient's birth_date
+-- Soln
+SELECT DISTINCT p.patient_id,
+	CONCAT(p.patient_id,
+		   LEN(p.last_name),
+		   YEAR(birth_date)) AS temp_pass
+FROM patients p
+JOIN admissions a ON p.patient_id = a.patient_id;
+
+-- Q. 47 Each admission costs $50 for patients without insurance, and $10 for patients with insurance.
+--       All patients with an even patient_id have insurance. Give each patient a 'Yes' if they have insurance,
+--       and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group
+-- Soln
+SELECT 
+CASE 
+     WHEN patient_id % 2 = 0 Then 'Yes'
+     ELSE  'No' 
+END as has_insurance,
+SUM(CASE
+         WHEN patient_id % 2 = 0 Then 10
+         ELSE 50 
+    END) as cost_after_insurance
+FROM admissions 
+GROUP BY has_insurance;
+
+-- Q.48 Show the provinces that has more patients identified as 'M' than 'F'. Must only show full province_name
+-- Soln
+SELECT province_name
+FROM province_names pn
+JOIN patients p 
+ON pn.province_id = p.province_id
+GROUP BY province_name
+HAVING SUM(p.gender = 'M') > SUM(p.gender = 'F'); 
+-- or
+SELECT pr.province_name
+FROM patients AS pa
+JOIN province_names AS pr ON pa.province_id = pr.province_id
+GROUP BY pr.province_name
+HAVING COUNT( CASE WHEN gender = 'M' THEN 1 END) > COUNT( CASE WHEN gender = 'F' THEN 1 END);      
+
+-- Q.49 We are looking for a specific patient. Pull all columns for the patient who matches the following criteria:
+-- First_name contains an 'r' after the first two letters.
+-- Identifies their gender as 'F'
+-- Born in February, May, or December
+-- Their weight would be between 60kg and 80kg
+-- Their patient_id is an odd number
+-- They are from the city 'Kingston'
+-- Soln
+SELECT *
+FROM patients
+WHERE  first_name LIKE '__r%' 
+   AND gender = 'F'
+   AND MONTH(birth_date) IN (2 , 5, 12)
+   AND weight BETWEEN 60 AND 80
+   AND patient_id % 2 = 1
+   AND city = 'Kingston';
+
+-- Q.50 Show the percent of patients that have 'M' as their gender. Round the answer to the nearest hundreth number and in percent form.
+-- output: 54.48%
+-- Soln
+SELECT CONCAT(ROUND(((SELECT COUNT(*) FROM patients WHERE gender = 'M')*100)/CAST(COUNT(*) AS float),2),'%') AS percent_of_male_patients FROM patients;
+
+-- Q.51 For each day display the total amount of admissions on that day. Display the amount changed from the previous date
+-- Soln
+SELECT admission_date, COUNT(admission_date) AS admission_day,
+COUNT(admission_date) - LAG(COUNT(admission_date)) OVER(ORDER BY admission_date) AS admission_count_change
+FROM admissions 
+GROUP BY admission_date;
+
+-- Q.52 Sort the province names in ascending order in such a way that the province 'Ontario' is always on top.
+-- Soln
+SELECT province_name
+FROM province_names
+ORDER BY (CASE
+              WHEN province_name = 'Ontario' THEN 0
+			  ELSE 1
+		  END), province_name;       
+
+-- Q.53  We need a breakdown for the total amount of admissions each doctor has started each year.
+--       Show the doctor_id, doctor_full_name, specialty, year, total_admissions for that year.
+-- Soln
+select doctor_id,
+concat(first_name," ",last_name) as doctor_name,
+specialty,
+year(admission_date) as selected_year,
+count(*) as total_admissions
+from doctors d 
+join admissions a 
+on d.doctor_id = a.attending_doctor_id
+group by doctor_name,selected_year;
